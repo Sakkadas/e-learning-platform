9c3c1d24dd81201a6c3b09f91fad02279ffac876
IlyaNesterov <Eildos95@gmail.com>
2021-04-27 19:36:53 +0300

    Modified CourseDetailView

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -13,6 +13,7 @@ from django.views.generic.detail import DetailView
 
 from .models import Course, Module, Content
 from .forms import ModuleFormSet
+from students.forms import CourseEnrollForm
 
 
 class OwnerMixin(object):
@@ -194,3 +195,11 @@ class CourseListView(TemplateResponseMixin, View):
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['enroll_form'] = CourseEnrollForm(
+            initial={'course': self.object})
+
+        return context
