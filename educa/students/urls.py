b8f7916fb284d426f5abebdf9d2e601aa9e85dbb
IlyaNesterov <Eildos95@gmail.com>
2021-04-28 16:37:50 +0300

    Implemented cache features

--- educa/students/urls.py
+++ educa/students/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from . import views
 
 urlpatterns = [
@@ -12,9 +14,9 @@ urlpatterns = [
          views.StudentCourseListView.as_view(),
          name='student_course_list'),
     path('course/<pk>/',
-         views.StudentCourseDetailView.as_view(),
+         cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
          name='student_course_detail'),
     path('course/<pk>/<module_id>/',
-         views.StudentCourseDetailView.as_view(),
+         cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
          name='student_course_detail_module'),
 ]
