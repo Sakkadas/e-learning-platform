70dac2e1faf90e05e2ccfdcc29422d63f6f781af
IlyaNesterov <Eildos95@gmail.com>
2021-04-29 16:58:37 +0300

    Added view for users to enroll on courses

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
 from ..models import Subject
 from .serializers import SubjectSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from ..models import Course
 
 
 class SubjectListView(generics.ListAPIView):
@@ -11,3 +15,10 @@ class SubjectListView(generics.ListAPIView):
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+
+class CourseEnrollView(APIView):
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
