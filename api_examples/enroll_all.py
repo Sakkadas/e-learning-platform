2739cbb4fbf9dec2e6f35a76d0ce9e2862820bb6
IlyaNesterov <Eildos95@gmail.com>
2021-04-29 18:06:25 +0300

    Created file that test API (Available courses, Enroll course)

---
+++ api_examples/enroll_all.py
@@ -0,0 +1,22 @@
+import requests
+
+base_url = 'http://127.0.0.1:8000/api/'
+
+#username = 'test'
+#password = '12345'
+
+# retrieve all courses
+r = requests.get(f'{base_url}courses/')
+courses = r.json()
+
+available_courses = ', '.join([course['title'] for course in courses])
+print(f'Available courses: {available_courses}')
+
+# for course in courses:
+#     course_id = course['id']
+#     course_title = course['title']
+#     r = requests.post(f'{base_url}courses/{course_id}/enroll/',
+#                       auth=(username, password))
+#     if r.status_code == 200:
+#         # successful request
+#         print(f'Successfully enrolled in {course_title}')
