f6b07960d106e9bbf9bbd15209aa028d8e73db84
IlyaNesterov <Eildos95@gmail.com>
2021-04-29 16:59:31 +0300

    Added url path for CourseEnrollView

--- educa/courses/api/urls.py
+++ educa/courses/api/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('subjects/<pk>/',
          views.SubjectDetailView.as_view(),
          name='subject_detail'),
+    path('courses/<pk>/enroll/',
+         views.CourseEnrollView.as_view(),
+         name='course_enroll'),
 ]
