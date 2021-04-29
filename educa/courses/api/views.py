25168724fa234555c1792d800af5ade635ec7f97
IlyaNesterov <Eildos95@gmail.com>
2021-04-29 17:23:37 +0300

    Added `action` decorator and `enroll` method to CourseViewSet

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -5,11 +5,11 @@ from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from .serializers import CourseSerializer
-
+from .serializers import SubjectSerializer
 from ..models import Course
 from ..models import Subject
-from .serializers import SubjectSerializer
 
 
 class SubjectListView(generics.ListAPIView):
@@ -35,3 +35,12 @@ class CourseEnrollView(APIView):
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    @action(detail=True,
+            methods=['post'],
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
