611ec6dec0038dcda74eb203fd93ab289979c9cb
IlyaNesterov <Eildos95@gmail.com>
2021-04-29 17:13:25 +0300

    Created viewset for the Course model

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -4,6 +4,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from .serializers import CourseSerializer
 
 from ..models import Course
 from ..models import Subject
@@ -23,7 +25,13 @@ class SubjectDetailView(generics.RetrieveAPIView):
 class CourseEnrollView(APIView):
     authentication_classes = (BasicAuthentication,)
     permission_classes = (IsAuthenticated,)
+
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
