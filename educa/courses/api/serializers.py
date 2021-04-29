2481f782fb4fbc478e5fd67062c83d74ea196855
IlyaNesterov <Eildos95@gmail.com>
2021-04-29 17:44:27 +0300

    Extended serializers for Content, Module and Course models

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from ..models import Subject
 from ..models import Course, Module
+from ..models import Content
 
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -22,3 +23,33 @@ class CourseSerializer(serializers.ModelSerializer):
         model = Course
         fields = ['id', 'subject', 'title', 'slug', 'overview',
                   'created', 'owner', 'modules']
+
+
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = ['order', 'item']
+
+
+class ModuleWithContentsSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description', 'contents']
+
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    modules = ModuleWithContentsSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug',
+                  'overview', 'created', 'owner', 'modules']
