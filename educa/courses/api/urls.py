b899f875446c6c401c4892b8c1135e5d7ad70357
IlyaNesterov <Eildos95@gmail.com>
2021-04-29 17:16:54 +0300

    Added router for generating URLs automatically for viewset

--- educa/courses/api/urls.py
+++ educa/courses/api/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
+
+router = routers.DefaultRouter()
+router.register('courses', views.CourseViewSet)
 
 app_name = 'courses'
 
@@ -13,4 +17,5 @@ urlpatterns = [
     path('courses/<pk>/enroll/',
          views.CourseEnrollView.as_view(),
          name='course_enroll'),
+    path('', include(router.urls)),
 ]
