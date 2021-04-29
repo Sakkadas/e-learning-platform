bba44fbf571756c8d34f3bcdbb46e958d5bb36aa
IlyaNesterov <Eildos95@gmail.com>
2021-04-29 17:54:20 +0300

    Extended behavior of CourseViewSet

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -8,8 +8,10 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from .serializers import CourseSerializer
 from .serializers import SubjectSerializer
+from .serializers import CourseWithContentsSerializer
 from ..models import Course
 from ..models import Subject
+from .permissions import IsEnrolled
 
 
 class SubjectListView(generics.ListAPIView):
@@ -34,3 +36,11 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+    @action(detail=True,
+            methods=['get'],
+            serializer_class=CourseWithContentsSerializer,
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
