32889d934f560c13935b7b0f47cc2b04407be63b
IlyaNesterov <Eildos95@gmail.com>
2021-04-29 16:53:23 +0300

    Serializers for module and course views

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
 from ..models import Subject
+from ..models import Course, Module
 
 
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ['id', 'title', 'slug']
+
+
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    modules = ModuleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug', 'overview',
+                  'created', 'owner', 'modules']
