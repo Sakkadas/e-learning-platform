f7ad8c9c05d180ff5cff6f4c06df8a979d13e231
Sakkadas <Eildos95@gmail.com>
2021-05-09 20:16:09 +0300

    Modified `connect`,`disconnect` methods

--- educa/chat/consumers.py
+++ educa/chat/consumers.py
@@ -1,15 +1,25 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
-
+from asgiref.sync import async_to_sync
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.id = self.scope['url_route']['kwargs']['course_id']
+        self.room_group_name = 'chat_%s' % self.id
+        # join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
         # accept connection
         self.accept()
 
     def disconnect(self, close_code):
-        pass
-
+        # leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
     # receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
